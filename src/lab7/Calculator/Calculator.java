06651f1f673350982a8e51b68767d42a636be44b
cosminciocan <ciocan.cosmin@gmail.com>
2019-11-06 20:33:29 +0200

    Lab 7

---
+++ src/lab7/Calculator/Calculator.java
@@ -0,0 +1,12 @@
+package lab7.Calculator;
+
+public class Calculator {
+
+    public int sum(int f, int s){
+        return f + s;
+    }
+
+    public int substract(int f, int s){
+        return f - s;
+    }
+}
--- src/lab7/Calculator/Main.java
+++ src/lab7/Calculator/Main.java
@@ -1,20 +1,13 @@
 package lab7.Calculator;
 
 
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-
-        Read read = new Read();
-
-       int a = read.getInt();
-
-
-        System.out.println("Asta e valoarea: " + a);
+        Program program = new Program();
+        program.runProgram();
 
 
     }
